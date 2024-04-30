8012436ba27f434205ce95ef6ab006d30acd0741
Denis Kulicek <5512606+deniskulicek@users.noreply.github.com>
2024-04-30 20:47:00 +0200

    Add Comment schema with migration

---
+++ lib/pinboard/posts/comment.ex
@@ -0,0 +1,22 @@
+defmodule Pinboard.Posts.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Pinboard.Accounts.User
+  alias Pinboard.Posts.Post
+
+  schema "comments" do
+    field :body, :string
+    belongs_to :user, User
+    belongs_to :post, Post
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+  end
+end
