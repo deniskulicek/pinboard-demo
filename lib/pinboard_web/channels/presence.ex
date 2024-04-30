74b409110182b727e7bd8941b552c96b43a74b71
Denis Kulicek <5512606+deniskulicek@users.noreply.github.com>
2024-04-30 22:19:42 +0200

    Refactor presence diffing

--- lib/pinboard_web/channels/presence.ex
+++ lib/pinboard_web/channels/presence.ex
@@ -1,4 +1,6 @@
 defmodule PinboardWeb.Presence do
+  import Phoenix.Component, only: [assign: 3]
+
   @moduledoc """
   Provides presence tracking to channels and processes.
 
@@ -8,4 +10,66 @@ defmodule PinboardWeb.Presence do
   use Phoenix.Presence,
     otp_app: :pinboard,
     pubsub_server: Pinboard.PubSub
+
+  # Presence diffing helpers
+  def handle_diff(socket, diff) do
+    socket
+    # we don't actually need the diff, but it's useful for debugging
+    |> assign(:diff, diff)
+    |> remove_presences(diff.leaves)
+    |> add_presences(diff.joins)
+  end
+
+  defp remove_presences(socket, leaves) do
+    presences = socket.assigns.presences
+
+    left_refs =
+      Enum.map(leaves, fn {_user_id, %{metas: metalist}} ->
+        Enum.map(metalist, & &1.phx_ref)
+      end)
+      |> List.flatten()
+
+    reject_left_refs = fn metas ->
+      Enum.reject(metas, fn entry ->
+        Enum.member?(left_refs, entry.phx_ref)
+      end)
+    end
+
+    updated_presences =
+      presences
+      |> Enum.reduce(%{}, fn {user_id, %{metas: metas}}, acc ->
+        new_metas = reject_left_refs.(metas)
+
+        if(Enum.empty?(new_metas)) do
+          acc
+        else
+          Map.put(acc, user_id, %{metas: new_metas})
+        end
+      end)
+
+    assign(socket, :presences, updated_presences)
+  end
+
+  defp add_presences(socket, joins) do
+    presences = socket.assigns.presences
+
+    updated_presences =
+      Enum.map(joins, fn {user_id, %{metas: metas}} ->
+        prev_metas = Map.get(presences, user_id)
+
+        new_metas =
+          if(prev_metas) do
+            # additional user presence session
+            [metas | prev_metas.metas] |> List.flatten() |> Enum.uniq()
+          else
+            # new user presence session
+            metas
+          end
+
+        {user_id, %{metas: new_metas}}
+      end)
+      |> Enum.into(%{})
+
+    assign(socket, :presences, Map.merge(presences, updated_presences))
+  end
 end
--- lib/pinboard_web/live/feed_live/index.ex
+++ lib/pinboard_web/live/feed_live/index.ex
@@ -160,70 +160,14 @@ defmodule PinboardWeb.FeedLive.Index do
   end
 
   # Presence updates
-  # Handle presence updates
   def handle_info(%{event: "presence_diff", payload: diff}, socket) do
     socket =
       socket
-      |> assign(:diff, diff)
-      |> remove_presences(diff.leaves)
-      |> add_presences(diff.joins)
-      # reload posts to update their user presence statuses
+      |> Presence.handle_diff(diff)
       |> stream(:posts, Posts.list_all())
 
+    # we reloaded posts here to get updated user presence status in the UI
     {:noreply, socket}
-  end
-
-  defp remove_presences(socket, leaves) do
-    presences = socket.assigns.presences
-
-    left_refs =
-      Enum.map(leaves, fn {_user_id, %{metas: metalist}} ->
-        Enum.map(metalist, & &1.phx_ref)
-      end)
-      |> List.flatten()
-
-    reject_left_refs = fn metas ->
-      Enum.reject(metas, fn entry ->
-        Enum.member?(left_refs, entry.phx_ref)
-      end)
-    end
-
-    updated_presences =
-      presences
-      |> Enum.reduce(%{}, fn {user_id, %{metas: metas}}, acc ->
-        new_metas = reject_left_refs.(metas)
-
-        if(Enum.empty?(new_metas)) do
-          acc
-        else
-          Map.put(acc, user_id, %{metas: new_metas})
-        end
-      end)
-
-    assign(socket, :presences, updated_presences)
-  end
-
-  defp add_presences(socket, joins) do
-    presences = socket.assigns.presences
-
-    updated_presences =
-      Enum.map(joins, fn {user_id, %{metas: metas}} ->
-        prev_metas = Map.get(presences, user_id)
-
-        new_metas =
-          if(prev_metas) do
-            # additional user presence session
-            [metas | prev_metas.metas] |> List.flatten() |> Enum.uniq()
-          else
-            # new user presence session
-            metas
-          end
-
-        {user_id, %{metas: new_metas}}
-      end)
-      |> Enum.into(%{})
-
-    assign(socket, :presences, Map.merge(presences, updated_presences))
   end
 
   # https://hexdocs.pm/phoenix_live_view/uploads.html#consume-uploaded-entries
