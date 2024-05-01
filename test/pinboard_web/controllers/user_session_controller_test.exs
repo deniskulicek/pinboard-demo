3e5d563b4e711d459974c833b3f98c81a264b9ef
Denis Kulicek <5512606+deniskulicek@users.noreply.github.com>
2024-05-01 10:07:32 +0200

    Update default redirect path in failing tests

--- test/pinboard_web/controllers/user_session_controller_test.exs
+++ test/pinboard_web/controllers/user_session_controller_test.exs
@@ -15,7 +15,7 @@ defmodule PinboardWeb.UserSessionControllerTest do
         })
 
       assert get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/feed"
 
       # Now do a logged in request and assert on the menu
       conn = get(conn, ~p"/")
@@ -36,7 +36,7 @@ defmodule PinboardWeb.UserSessionControllerTest do
         })
 
       assert conn.resp_cookies["_pinboard_web_user_remember_me"]
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/feed"
     end
 
     test "logs the user in with return to", %{conn: conn, user: user} do
@@ -65,7 +65,7 @@ defmodule PinboardWeb.UserSessionControllerTest do
           }
         })
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/feed"
       assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Account created successfully"
     end
 
--- test/pinboard_web/live/user_registration_live_test.exs
+++ test/pinboard_web/live/user_registration_live_test.exs
@@ -17,7 +17,7 @@ defmodule PinboardWeb.UserRegistrationLiveTest do
         conn
         |> log_in_user(user_fixture())
         |> live(~p"/users/register")
-        |> follow_redirect(conn, "/")
+        |> follow_redirect(conn, "/feed")
 
       assert {:ok, _conn} = result
     end
@@ -45,7 +45,7 @@ defmodule PinboardWeb.UserRegistrationLiveTest do
       render_submit(form)
       conn = follow_trigger_action(form, conn)
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/feed"
 
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
